c8ad1687798e59c750a054ff47aece91c1dfaefb
Ananya <ananya.lahiri@onfido.com>
2024-07-24 15:54:03 +0100

    small change to SiglipOutput class to incl position_embedding

--- src/transformers/models/siglip/modeling_tf_siglip.py
+++ src/transformers/models/siglip/modeling_tf_siglip.py
@@ -148,25 +148,21 @@ class TFSiglipVisionEmbeddings(keras.layers.Layer):
         self.num_patches = (self.image_size // self.patch_size) ** 2
         self.num_positions = self.num_patches + 1
 
-        self.config = config
-
-        self.patch_embedding = keras.layers.Conv2D(
-            filters=self.embed_dim,
-            kernel_size=self.patch_size,
-            strides=self.patch_size,
-            padding="valid",
-            data_format="channels_last",
-            use_bias=False,
-            kernel_initializer=get_initializer(self.config.initializer_range * self.config.initializer_factor),
-            name="patch_embedding",
+        self.position_embedding = keras.layers.Embedding(
+            input_dim=self.num_positions,
+            output_dim=self.embed_dim,
+            embeddings_initializer=get_initializer(self.config.initializer_range),
+            name="position_embedding",
         )
+
+        self.position_ids = tf.range(start=0, limit=self.num_positions)
 
     def build(self, input_shape: tf.TensorShape = None):
         factor = self.config.initializer_factor
 
         self.class_embedding = self.add_weight(
             shape=(self.embed_dim,),
-            initializer=get_initializer(self.embed_dim ** -0.5 * factor),
+            initializer=get_initializer(self.embed_dim**-0.5 * factor),
             trainable=True,
             name="class_embedding",
         )
